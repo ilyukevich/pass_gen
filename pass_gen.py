1f66f0c2e6a538733e32571ed6af51e782ccc773
Artsiom Ilyukevich <58914529+ilyukevich@users.noreply.github.com>
2020-02-29 23:49:05 +0300

    Update pass_gen.py

--- pass_gen.py
+++ pass_gen.py
@@ -1,6 +1,6 @@
 # coding=utf-8
-# в процессе
-# перебор всех возможных комбинаций из 4х списков:
+# in the process
+# enumeration of all possible combinations from 4 lists:
 
 import random
 
@@ -10,21 +10,21 @@ class Db(object):
         self.unit = unit
         self.create = unit * 4
 
+    #in the process
     def __str__(self):
         pass
-
 
     def meth_create(self):
         forbidden_symbols = ["%", "`", " ", ".", ","]
         self.create = self.unit - 1
         self.list = []
         for i in range(self.unit):
-            n = input(">>> Перечислите символы для генерации пароля в списке:" )
-            print("У вас сталось символов для данного списка:", self.create)
+            n = input(">>> List the characters for generating the password in the list:" )
+            print("You have characters for this list:", self.create)
             if n not in forbidden_symbols:
                 self.list.append(n)
             else:
-                print("Недопустимые символы!")
+                print("Unacceptable symbols!")
                 self.unit += 1
             self.create -= 1
         return self.list
@@ -48,40 +48,40 @@ class Db(object):
 
 def ask(question, list_gen):
     response = None
-    while response not in ("да", "нет"):
+    while response not in ("yes", "no"):
         response = input(question).lower()
-        if response == "да":
+        if response == "yes":
             print()
             #print(list_gen)
             choice = random.choice(list_gen)
-            print("Ваш случайно выбранный пароль:")
+            print("Your randomly selected password:")
             print(choice)
-        elif response == "нет":
-            print("До свидания. Всего хорошего.")
+        elif response == "no":
+            print("Bye. Good luck.")
         else:
-            print("Ответ 'да' или 'нет'. Попробуйте еще раз!")
+            print("The answer is 'yes' or 'no'. Try again!")
 
 def main():
     print()
-    print("\tПрограмма генерации пароля из пользовательских символов")
+    print("\tUser character password program")
     print()
-    unit = int(input(">>> Количество символов в каждом списке?: "))
+    unit = int(input(">>> The number of characters in each list?: "))
     db1 = Db(unit)
     db2 = Db(unit)
     db3 = Db(unit)
     db4 = Db(unit)
-    print("\tГенерируем первый список символов:")
+    print("\tGenerate the first list of characters:")
     l1 = db1.meth_create()
-    print("\tГенерируем второй список символов: ")
+    print("\tGenerate the second list of characters: ")
     l2 = db2.meth_create()
-    print("\tГенерируем третий список символов:")
+    print("\tGenerate the third list of characters:")
     l3 = db3.meth_create()
-    print("\tГенерируем четвертый список символов:")
+    print("\tGenerate the fourth list of characters:")
     l4 = db4.meth_create()
     print()
-    print("Выведены все возможные комбинации из ваших символов:")
+    print("All possible combinations of the characters that you entered are generated:")
     list_gen = db1.meth_generation(l1, l2, l3, l4)
     print(list_gen)
-    ask(">>> Хотите выбрать случайный пароль из сформированных комбинаций? (да/нет) : ", list_gen)
+    ask(">>> Want to choose a random password from the generated combinations? (yes/no) : ", list_gen)
 
 main()
